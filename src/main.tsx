cde91ebb6b2f05d2dd339b2fc87587f260a5be6c
Truckers123 <info@sqp-ventures.com>
2025-07-13 19:27:04 +0700

    Fix package.json - restore complete dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import { createRoot } from 'react-dom/client'
 import './shadcn.css'
 import App from './App'
 
-const root = createRoot(document.getElementById('app')!)
+const root = createRoot(document.getElementById('root')!)
 root.render(<App />)
